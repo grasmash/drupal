a7916744b0384c8498f831043a686d56c41c3a63
Matthew Grasmick <matthew.grasmick@acquia.com>
2014-04-26 17:28:42 -0400

    Removing install step for Behat. Updating run-behat-tests.sh.

--- core/scripts/run-behat-tests.sh
+++ core/scripts/run-behat-tests.sh
@@ -1,4 +1,13 @@
 #! /bin/bash -e
+# Utilize this command by running ./run-behat-tests.sh https://your.site.url
+# You may append additional arguments or flags accepted by the behat binary.
+# E.g., ./run-behat-tests.sh https://your.site.url --tags="~@javascript"
+
+if (( $# < 1 ))
+then
+  echo "Usage: The first argument must be the base_url of the site to test. Subsequent argument will be passed directly to Behat"
+  exit 1
+fi
 
 _drupal_root() {
   # Go up until we find index.php
@@ -15,5 +24,17 @@ _drupal_root() {
   fi
 }
 
+# Set base_url to the first argument and then shift argument off of list.
+base_url=$1
+shift
+
+# Set drupal_root.
 drupal_root=`_drupal_root` && \
-$drupal_root/core/vendor/bin/behat -c $drupal_root/core/behat.yml
+
+# Set default Behat parameters. These can be overridden if a custom behat.yml
+# file is specified with the -c parameter.
+export BEHAT_PARAMS="extensions[Drupal\DrupalExtension\Extension][drupal][drupal_root]=$drupal_root&extensions[Behat\MinkExtension\Extension][base_url]=$base_url"
+
+cd $drupal_root/core
+$drupal_root/core/vendor/bin/behat "$@"
+
